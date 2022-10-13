9a82ac704d3e2e2237afec4a399bc9caca346af6
clambert-ign <craig.lambert@ignifi.co.uk>
2022-10-13 12:45:02 +0100

    AcousticContent
    Added Flexible Text Component

--- lib/components/globalFooter/GlobalFooter.jsx
+++ lib/components/globalFooter/GlobalFooter.jsx
@@ -94,9 +94,7 @@ const GlobalFooter = (props) => {
         </ul>
       </div>
       <div className={styles['footer-signOff']}>
-
-        CTCL Answers has been developed by Kyowa Kirin in partnership with Healthcare Professionals and Patient Advocacy Groups. CTCL Answers is the property of Kyowa Kirin International PLC.<br/>All rights reserved.
-
+      {footerSignoff}
       </div>
     </footer>
     <Modal show={openModal} title={t('pleaseSelectYourCountryOrRegion')} onClose={setOpenModal}>
